8fa7f90470e14dd813093dd9b4993c0eeacea127
Shao0203 <nzsyl0203@gmail.com>
2026-08-15 16:05:30 +0800

    import trainer and utility

--- app.py
+++ app.py
@@ -1,12 +1,9 @@
 import numpy as np
 
 
-W1 = np.random.randn(2, 4)
-b1 = np.random.randn(4)
-x = np.random.randn(10, 2)
-h = np.dot(x, W1) + b1
-print(h.shape, h.ndim)
-print(h)
-
-print(1 / (1 + np.exp(-h)))  # sigmoid
-print(np.maximum(0, h))  # relu
+a = np.random.randn(3).astype('f')
+print(a.dtype)
+b = np.random.randn(3).astype(np.float32)
+print(b.dtype)
+c = np.random.randn(3).astype(np.float16)
+print(c.dtype)
