002411b0df33d814c72e867a9bbfdb79ed33229c
Shao0203 <nzsyl0203@gmail.com>
2026-08-30 08:06:46 +0800

    Implement time layers for Affine | SoftmaxWithLoss | SigmoidWithLoss | Embedding.

--- common/time_layers.py
+++ common/time_layers.py
@@ -42,6 +42,13 @@ class TimeRNN:
         self.stateful = stateful
 
     def forward(self, xs):
+        '''
+        xs[:, 0, :] → RNN → h0 → 存到 hs[:, 0, :]
+        xs[:, 1, :] → RNN → h1 → 存到 hs[:, 1, :]
+        xs[:, 2, :] → RNN → h2 → 存到 hs[:, 2, :]
+        ...
+        xs[:, 9, :] → RNN → h9 → 存到 hs[:, 9, :]
+        '''
         Wx, Wh, b = self.params
         N, T, D = xs.shape
         D, H = Wx.shape
@@ -58,13 +65,6 @@ class TimeRNN:
             self.layers.append(layer)
 
         return hs
-        '''
-        xs[:, 0, :] → RNN → h0 → 存到 hs[:, 0, :]
-        xs[:, 1, :] → RNN → h1 → 存到 hs[:, 1, :]
-        xs[:, 2, :] → RNN → h2 → 存到 hs[:, 2, :]
-        ...
-        xs[:, 9, :] → RNN → h9 → 存到 hs[:, 9, :]
-        '''
 
     def backward(self, dhs):
         Wx, Wh, b = self.params
@@ -93,3 +93,212 @@ class TimeRNN:
 
     def reset_state(self):
         self.h = None
+
+
+class Simple_TimeAffine:
+    def __init__(self, W, b):
+        self.W, self.b = W, b
+        self.dW, self.db = None, None
+        self.layers = None
+
+    def forward(self, xs):
+        N, T, D = xs.shape
+        D, M = self.W.shape
+
+        self.layers = []
+        out = np.empty((N, T, M), dtype='f')
+        for t in range(T):
+            layer = Affine(self.W, self.b)
+            out[:, t, :] = layer.forward(xs[:, t, :])
+            self.layers.append(layer)
+
+        return out
+
+    def backward(self, dout):
+        N, T, M = dout.shape
+        D, M = self.W.shape
+
+        dxs = np.empty((N, T, D), dtype='f')
+        self.dW, self.db = 0, 0
+        for t in range(T):
+            layer = self.layers[t]
+            dxs[:, t, :] = layer.backward(dout[:, t, :])
+
+            self.dW += layer.dW
+            self.db += layer.db
+
+        return dxs
+
+
+class Simple_TimeSoftmaxWithLoss:
+    def __init__(self):
+        self.params, self.grads = [], []
+        self.cache = None
+
+    def forward(self, xs, ts):
+        N, T, V = xs.shape
+        layers = []
+        loss = 0
+
+        for t in range(T):
+            layer = SoftmaxWithLoss()
+            loss += layer.forward(xs[:, t, :], ts[:, t])
+            layers.append(layer)
+        loss /= T
+
+        self.cache = (layers, xs)
+        return loss
+
+    def backward(self, dout=1):
+        layers, xs = self.cache
+        N, T, V = xs.shape
+        dxs = np.empty(xs.shape, dtype='f')
+
+        dout *= 1/T
+        for t in range(T):
+            layer = layers[t]
+            dxs[:, t, :] = layer.backward(dout)
+
+        return dxs
+
+
+class TimeAffine:
+    def __init__(self, W, b):
+        self.params = [W, b]
+        self.grads = [np.zeros_like(W), np.zeros_like(b)]
+        self.x = None
+
+    def forward(self, x):
+        N, T, D = x.shape
+        W, b = self.params
+
+        rx = x.reshape(N*T, -1)
+        out = np.dot(rx, W) + b
+        self.x = x
+        return out.reshape(N, T, -1)
+
+    def backward(self, dout):
+        x = self.x
+        N, T, D = x.shape
+        W, b = self.params
+
+        dout = dout.reshape(N*T, -1)
+        rx = x.reshape(N*T, -1)
+
+        db = np.sum(dout, axis=0)
+        dW = np.dot(rx.T, dout)
+        dx = np.dot(dout, W.T)
+        dx = dx.reshape(*x.shape)
+
+        self.grads[0][...] = dW
+        self.grads[1][...] = db
+
+        return dx
+
+
+class TimeSoftmaxWithLoss:
+    def __init__(self):
+        self.params, self.grads = [], []
+        self.cache = None
+        self.ignore_label = -1
+
+    def forward(self, xs, ts):
+        N, T, V = xs.shape
+
+        if ts.ndim == 3:  # 在监督标签为one-hot向量的情况下
+            ts = ts.argmax(axis=2)
+
+        mask = (ts != self.ignore_label)
+
+        # 按批次大小和时序大小进行整理（reshape）
+        xs = xs.reshape(N * T, V)
+        ts = ts.reshape(N * T)
+        mask = mask.reshape(N * T)
+
+        ys = softmax(xs)
+        ls = np.log(ys[np.arange(N * T), ts])
+        ls *= mask  # 与ignore_label相应的数据将损失设为0
+        loss = -np.sum(ls)
+        loss /= mask.sum()
+
+        self.cache = (ts, ys, mask, (N, T, V))
+        return loss
+
+    def backward(self, dout=1):
+        ts, ys, mask, (N, T, V) = self.cache
+
+        dx = ys
+        dx[np.arange(N * T), ts] -= 1
+        dx *= dout
+        dx /= mask.sum()
+        dx *= mask[:, np.newaxis]  # 与ignore_label相应的数据将梯度设为0
+
+        dx = dx.reshape((N, T, V))
+
+        return dx
+
+
+class TimeSigmoidWithLoss:
+    def __init__(self):
+        self.params, self.grads = [], []
+        self.xs_shape = None
+        self.layers = None
+
+    def forward(self, xs, ts):
+        N, T = xs.shape
+        self.xs_shape = xs.shape
+
+        self.layers = []
+        loss = 0
+
+        for t in range(T):
+            layer = SigmoidWithLoss()
+            loss += layer.forward(xs[:, t], ts[:, t])
+            self.layers.append(layer)
+
+        return loss / T
+
+    def backward(self, dout=1):
+        N, T = self.xs_shape
+        dxs = np.empty(self.xs_shape, dtype='f')
+
+        dout *= 1/T
+        for t in range(T):
+            layer = self.layers[t]
+            dxs[:, t] = layer.backward(dout)
+
+        return dxs
+
+
+class TimeEmbedding:
+    def __init__(self, W):
+        self.params = [W]
+        self.grads = [np.zeros_like(W)]
+        self.layers = None
+        self.W = W
+
+    def forward(self, xs):
+        N, T = xs.shape
+        V, D = self.W.shape
+
+        out = np.empty((N, T, D), dtype='f')
+        self.layers = []
+
+        for t in range(T):
+            layer = Embedding(self.W)
+            out[:, t, :] = layer.forward(xs[:, t])
+            self.layers.append(layer)
+
+        return out
+
+    def backward(self, dout):
+        N, T, D = dout.shape
+
+        grad = 0
+        for t in range(T):
+            layer = self.layers[t]
+            layer.backward(dout[:, t, :])
+            grad += layer.grads[0]
+
+        self.grads[0][...] = grad
+        return None
