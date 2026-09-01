d39b857d8257eae912d051a57d0f1cf52bc2642b
Shao0203 <nzsyl0203@gmail.com>
2026-09-01 16:26:43 +0800

    Refactor time layers.

--- common/time_layers.py
+++ common/time_layers.py
@@ -60,9 +60,9 @@ class TimeRNN:
 
         for t in range(T):
             layer = RNN(*self.params)
+            self.layers.append(layer)
             self.h = layer.forward(xs[:, t, :], self.h)
             hs[:, t, :] = self.h
-            self.layers.append(layer)
 
         return hs
 
@@ -71,9 +71,10 @@ class TimeRNN:
         N, T, H = dhs.shape
         D, H = Wx.shape
 
-        dxs = np.empty((N, T, D), dtype='f')
         dh = 0
         grads = [0, 0, 0]
+        dxs = np.empty((N, T, D), dtype='f')
+
         for t in reversed(range(T)):
             layer = self.layers[t]
             dx, dh = layer.backward(dhs[:, t, :] + dh)
@@ -84,8 +85,8 @@ class TimeRNN:
 
         for i, grad in enumerate(grads):
             self.grads[i][...] = grad
-        self.dh = dh
 
+        self.dh = dh
         return dxs
 
     def set_state(self, h):
@@ -95,73 +96,6 @@ class TimeRNN:
         self.h = None
 
 
-class Simple_TimeAffine:
-    def __init__(self, W, b):
-        self.W, self.b = W, b
-        self.dW, self.db = None, None
-        self.layers = None
-
-    def forward(self, xs):
-        N, T, D = xs.shape
-        D, M = self.W.shape
-
-        self.layers = []
-        out = np.empty((N, T, M), dtype='f')
-        for t in range(T):
-            layer = Affine(self.W, self.b)
-            out[:, t, :] = layer.forward(xs[:, t, :])
-            self.layers.append(layer)
-
-        return out
-
-    def backward(self, dout):
-        N, T, M = dout.shape
-        D, M = self.W.shape
-
-        dxs = np.empty((N, T, D), dtype='f')
-        self.dW, self.db = 0, 0
-        for t in range(T):
-            layer = self.layers[t]
-            dxs[:, t, :] = layer.backward(dout[:, t, :])
-
-            self.dW += layer.dW
-            self.db += layer.db
-
-        return dxs
-
-
-class Simple_TimeSoftmaxWithLoss:
-    def __init__(self):
-        self.params, self.grads = [], []
-        self.cache = None
-
-    def forward(self, xs, ts):
-        N, T, V = xs.shape
-        layers = []
-        loss = 0
-
-        for t in range(T):
-            layer = SoftmaxWithLoss()
-            loss += layer.forward(xs[:, t, :], ts[:, t])
-            layers.append(layer)
-        loss /= T
-
-        self.cache = (layers, xs)
-        return loss
-
-    def backward(self, dout=1):
-        layers, xs = self.cache
-        N, T, V = xs.shape
-        dxs = np.empty(xs.shape, dtype='f')
-
-        dout *= 1/T
-        for t in range(T):
-            layer = layers[t]
-            dxs[:, t, :] = layer.backward(dout)
-
-        return dxs
-
-
 class TimeAffine:
     def __init__(self, W, b):
         self.params = [W, b]
@@ -169,26 +103,26 @@ class TimeAffine:
         self.x = None
 
     def forward(self, x):
-        N, T, D = x.shape
-        W, b = self.params
-
-        rx = x.reshape(N*T, -1)
-        out = np.dot(rx, W) + b
         self.x = x
-        return out.reshape(N, T, -1)
+        W, b = self.params
+        N, T, D = x.shape  # 5, 4, 3
+        D, M = W.shape  # 3, 10
+
+        rx = x.reshape(N * T, -1)  # 20, 3
+        out = np.dot(rx, W) + b  # 20, 10
+        return out.reshape(N, T, M)  # 5, 4, 10
 
     def backward(self, dout):
-        x = self.x
+        x = self.x  # 5, 4, 3
         N, T, D = x.shape
         W, b = self.params
 
-        dout = dout.reshape(N*T, -1)
-        rx = x.reshape(N*T, -1)
-
-        db = np.sum(dout, axis=0)
-        dW = np.dot(rx.T, dout)
-        dx = np.dot(dout, W.T)
-        dx = dx.reshape(*x.shape)
+        rx = x.reshape(N * T, -1)  # 20, 3
+        dout = dout.reshape(N * T, -1)  # 20, 10
+        dW = np.dot(rx.T, dout)  # np.dot((3, 20), (20, 10)) = (3, 10) same with W.shape(3, 10)
+        db = np.sum(dout, axis=0)  # (10, )
+        drx = np.dot(dout, W.T)  # np.dot((20, 10), (10, 3)) = (20, 3) same with rx.shape(20, 3)
+        dx = drx.reshape(*x.shape)  # 5, 4, 3 same as x.shape(5, 4, 3)
 
         self.grads[0][...] = dW
         self.grads[1][...] = db
@@ -203,10 +137,20 @@ class TimeSoftmaxWithLoss:
         self.ignore_label = -1
 
     def forward(self, xs, ts):
+        '''
+        # 忽略填充位置标签为-1的值, 不参加训练
+        ts = [[1, 2, 3, 4, -1, -1],   # 句子1, 后两个是 ignore
+              [5, 6, 7, 8, 9, -1]]    # 句子2, 最后一个是 ignore
+
+        mask = (ts != -1)
+        # [[True, True, True, True, False, False],
+        #  [True, True, True, True, True, False]]
+
+        '''
         N, T, V = xs.shape
 
-        if ts.ndim == 3:  # 在监督标签为one-hot向量的情况下
-            ts = ts.argmax(axis=2)
+        if ts.ndim == 3:  # one-hot 形式 (N, T, V)
+            ts = np.argmax(ts, axis=-1)  # 转成索引 (N, T)
 
         mask = (ts != self.ignore_label)
 
@@ -217,24 +161,20 @@ class TimeSoftmaxWithLoss:
 
         ys = softmax(xs)
         ls = np.log(ys[np.arange(N * T), ts])
-        ls *= mask  # 与ignore_label相应的数据将损失设为0
-        loss = -np.sum(ls)
-        loss /= mask.sum()
+        ls *= mask  # 与ignore_label相应的数据将损失设为0(乘以False(即 0)), 不参与训练
+        loss = -np.sum(ls) / mask.sum()
 
         self.cache = (ts, ys, mask, (N, T, V))
         return loss
 
     def backward(self, dout=1):
         ts, ys, mask, (N, T, V) = self.cache
-
         dx = ys
         dx[np.arange(N * T), ts] -= 1
-        dx *= dout
-        dx /= mask.sum()
-        dx *= mask[:, np.newaxis]  # 与ignore_label相应的数据将梯度设为0
-
+        dx *= dout / mask.sum()
+        # 与ignore_label相应的数据将梯度设为0。np.newaxis"加一个维度": mask=(N*T,); mask[:, np.newaxis]=(N*T, 1)
+        dx *= mask[:, np.newaxis]
         dx = dx.reshape((N, T, V))
-
         return dx
 
 
@@ -302,3 +242,70 @@ class TimeEmbedding:
 
         self.grads[0][...] = grad
         return None
+
+
+class Simple_TimeAffine:
+    def __init__(self, W, b):
+        self.W, self.b = W, b
+        self.dW, self.db = None, None
+        self.layers = None
+
+    def forward(self, xs):
+        N, T, D = xs.shape
+        D, M = self.W.shape
+
+        self.layers = []
+        out = np.empty((N, T, M), dtype='f')
+        for t in range(T):
+            layer = Affine(self.W, self.b)
+            out[:, t, :] = layer.forward(xs[:, t, :])
+            self.layers.append(layer)
+
+        return out
+
+    def backward(self, dout):
+        N, T, M = dout.shape
+        D, M = self.W.shape
+
+        dxs = np.empty((N, T, D), dtype='f')
+        self.dW, self.db = 0, 0
+        for t in range(T):
+            layer = self.layers[t]
+            dxs[:, t, :] = layer.backward(dout[:, t, :])
+
+            self.dW += layer.dW
+            self.db += layer.db
+
+        return dxs
+
+
+class Simple_TimeSoftmaxWithLoss:
+    def __init__(self):
+        self.params, self.grads = [], []
+        self.cache = None
+
+    def forward(self, xs, ts):
+        N, T, V = xs.shape
+        layers = []
+        loss = 0
+
+        for t in range(T):
+            layer = SoftmaxWithLoss()
+            loss += layer.forward(xs[:, t, :], ts[:, t])
+            layers.append(layer)
+        loss /= T
+
+        self.cache = (layers, xs)
+        return loss
+
+    def backward(self, dout=1):
+        layers, xs = self.cache
+        N, T, V = xs.shape
+        dxs = np.empty(xs.shape, dtype='f')
+
+        dout *= 1/T
+        for t in range(T):
+            layer = layers[t]
+            dxs[:, t, :] = layer.backward(dout)
+
+        return dxs
--- testing.py
+++
@@ -1,6 +0,0 @@
-dic_a = {'a': 'aaa', 'b': 'bbb', 'c': 'ccc'}
-x = [1, 2, 3]
-a1, b1, c1 = dic_a.values()
-print(a1, b1, c1)
-xx = list(dic_a.values())
-print(xx)
